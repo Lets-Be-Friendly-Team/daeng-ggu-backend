4d7f5b7a5e6c9f3a3cc1b91a147c575c1c11702b
heogeonho <geonh1016@gmail.com>
2024-11-14 10:12:41 +0900

    init: 프로젝트 초기화 및 기본 설정

---
+++ src/main/java/com/example/daengguubackend/DaengGuuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.daengguubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaengGuuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaengGuuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daengguubackend/DaengGuuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daengguubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaengGuuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
