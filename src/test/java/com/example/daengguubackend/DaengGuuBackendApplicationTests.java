8e76f0a777906217e2301185d3140b88bee74883
heogeonho <geonh1016@gmail.com>
2024-11-18 13:13:29 +0900

    chore: spotless plugin 코드 수정 및 적용

--- src/test/java/com/example/daengguubackend/DaengGuuBackendApplicationTests.java
+++ src/test/java/com/example/daengguubackend/DaengGuuBackendApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DaengGuuBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
