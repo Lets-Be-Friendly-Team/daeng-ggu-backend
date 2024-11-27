cf83e58d948bf473ed132810d0ba088b0c5a9f20
heogeonho <geonh1016@gmail.com>
2024-11-27 13:42:31 +0900

    Feat: ReservationRepository 생성 및 findAllByCustomerId 쿼리 작업

---
+++ src/main/java/com/ureca/reservation/infrastructure/ReservationRepository.java
@@ -0,0 +1,18 @@
+package com.ureca.reservation.infrastructure;
+
+import com.ureca.reservation.domain.Reservation;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    // 보호자 ID를 기준으로 예약 내역 조회
+    @Query(
+            "SELECT r FROM Reservation r "
+                    + "JOIN r.pet p "
+                    + "WHERE p.customer.customerId = :customerId")
+    List<Reservation> findAllByCustomerId(@Param("customerId") Long guardianId);
+}
