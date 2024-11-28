82775bb62191400c6fbd9f508db4d76ae45b82a6
heogeonho <geonh1016@gmail.com>
2024-11-28 11:21:53 +0900

    Feat: 예약 조회 정보 수정에 따른 코드 수정

--- src/main/java/com/ureca/reservation/application/ReservationService.java
+++ src/main/java/com/ureca/reservation/application/ReservationService.java
@@ -2,9 +2,11 @@ package com.ureca.reservation.application;
 
 import com.ureca.common.exception.ApiException;
 import com.ureca.common.exception.ErrorCode;
+import com.ureca.profile.domain.Designer;
 import com.ureca.profile.infrastructure.CommonCodeRepository;
 import com.ureca.reservation.domain.Reservation;
 import com.ureca.reservation.infrastructure.ReservationRepository;
+import com.ureca.reservation.presentation.dto.DesignerInfoDto;
 import com.ureca.reservation.presentation.dto.RequestDetailDto;
 import com.ureca.reservation.presentation.dto.ReservationHistoryResponseDto;
 import java.util.List;
@@ -45,23 +47,42 @@ public class ReservationService {
                                                         ? reservation.getMonitoringFee().intValue()
                                                         : null)
                                         .totalPayment(reservation.getTotalPayment().intValue())
+                                        .designerInfo(
+                                                buildDesignerInfoDto(reservation.getDesigner()))
+                                        .estimateDetail(
+                                                reservation.getEstimate() != null
+                                                        ? reservation
+                                                                .getEstimate()
+                                                                .getEstimateDetail()
+                                                        : null)
                                         .requestDetail(buildRequestDetailDto(reservation))
                                         .build())
                 .collect(Collectors.toList());
+    }
+
+    private DesignerInfoDto buildDesignerInfoDto(Designer designer) {
+        return DesignerInfoDto.builder()
+                .designerId(designer.getDesignerId())
+                .designerName(designer.getDesignerName())
+                .officialName(designer.getOfficialName())
+                .designerImgUrl(designer.getDesignerImgUrl())
+                .designerImgName(designer.getDesignerImgName())
+                .address1(designer.getAddress1())
+                .address2(designer.getAddress2())
+                .detailAddress(designer.getDetailAddress())
+                .introduction(designer.getIntroduction())
+                .workExperience(designer.getWorkExperience())
+                .businessNumber(designer.getBusinessNumber())
+                .businessIsVerified(designer.getBusinessIsVerified())
+                .build();
     }
 
     private RequestDetailDto buildRequestDetailDto(Reservation reservation) {
         if ("R2".equals(reservation.getReservationType()) && reservation.getRequest() != null) {
             // Auction 방식: Request 데이터 사용
             return RequestDetailDto.builder()
-                    .desiredService(
-                            commonCodeRepository
-                                    .findByCodeId(reservation.getRequest().getDesiredServiceCode())
-                                    .getCodeDesc())
-                    .lastGroomingDate(
-                            commonCodeRepository
-                                    .findByCodeId(reservation.getRequest().getLastGroomingDate())
-                                    .getCodeDesc())
+                    .desiredService(getCodeDesc(reservation.getRequest().getDesiredServiceCode()))
+                    .lastGroomingDate(getCodeDesc(reservation.getRequest().getLastGroomingDate()))
                     .isDelivery(reservation.getRequest().getIsDelivery())
                     .desiredRegion(reservation.getRequest().getDesiredRegion())
                     .isMonitoring(reservation.getRequest().getIsMonitoringIncluded())
@@ -71,18 +92,16 @@ public class ReservationService {
 
         // Direct 방식: Reservation 데이터 사용
         return RequestDetailDto.builder()
-                .desiredService(
-                        commonCodeRepository
-                                .findByCodeId(reservation.getDesiredService())
-                                .getCodeDesc())
-                .lastGroomingDate(
-                        commonCodeRepository
-                                .findByCodeId(reservation.getLastGroomingDate())
-                                .getCodeDesc())
+                .desiredService(getCodeDesc(reservation.getDesiredService()))
+                .lastGroomingDate(getCodeDesc(reservation.getLastGroomingDate()))
                 .isDelivery(reservation.getIsDelivery())
                 .desiredRegion(null) // Direct 예약은 지역 정보 없음
                 .isMonitoring(reservation.getIsMonitoring())
                 .additionalRequest(reservation.getAdditionalRequest())
                 .build();
+    }
+
+    private String getCodeDesc(String code) {
+        return commonCodeRepository.findByCodeId(code).getCodeDesc();
     }
 }
