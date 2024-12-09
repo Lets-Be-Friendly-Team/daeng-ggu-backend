0f390b61bab407cb60a9d4281973b88f974da23a
rhdqngkwk90 <ouorhdqngkwk90@gmail.com>
2024-12-10 04:00:42 +0900

    Fix: Cookie, JWT 적용에 따른 로그인 로직 수정

--- src/main/java/com/ureca/login/presentation/KakaoController.java
+++ src/main/java/com/ureca/login/presentation/KakaoController.java
@@ -1,9 +1,9 @@
 package com.ureca.login.presentation;
 
+import com.ureca.common.util.CookieUtil;
 import com.ureca.login.application.KakaoService;
 import com.ureca.login.application.LoginService;
 import com.ureca.login.presentation.dto.KakaoDTO;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -30,48 +30,42 @@ public class KakaoController {
     /**
      * @title 보호자 - 액세스 토큰 및 유저 정보 요청
      * @description 로그인 성공 시, 카카오 플랫폼 서버에서 인가 코드 가지고 리다이렉션 (callback) 인가 코드를 사용해서 토큰 발급을 요청
-     *     (getKakaoInfo) 토큰을 사용해서 사용자 정보 가져오기 요청 (getUserInfoWithToken) 사용자 정보를 반환
-     * @return kakaoInfo 유저 정보
+     *     (getKakaoInfo) 토큰을 사용해서 사용자 정보 가져오기 요청 (getUserInfoWithToken) 사용자 정보를 JWT에 담아서 리다이렉트
+     *     (generateJwtToken)
+     * @return JWT 담은 쿠키를 헤더에 담아 리다이렉트
      */
     @GetMapping("/callback")
     public void callback(HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         // service - 유저 정보 반환
         KakaoDTO kakaoInfo = kakaoService.getKakaoInfo(request.getParameter("code"), CUSTOMER);
+        // service - 토큰 발급
         String jwt = loginService.generateJwtToken(kakaoInfo);
+        // util - 쿠키 생성
+        String cookie = CookieUtil.createCookies(jwt);
 
-        // 쿠키 생성
-        Cookie cookie = new Cookie("token", jwt);
-        cookie.setHttpOnly(true); // 보안을 위해 HttpOnly 설정
-        cookie.setSecure(true); // HTTPS 환경에서만 쿠키를 전송하도록 설정 (필요에 따라)
-        cookie.setMaxAge(60 * 60); // 쿠키 유효 시간 설정 (1시간)
-        cookie.setPath("/"); // 모든 경로에서 접근할 수 있도록 설정
-        response.addCookie(cookie);
-
+        response.setHeader("Set-Cookie", cookie);
         response.sendRedirect(LOGIN_REDIRECT_URL);
     }
 
     /**
      * @title 디자이너 - 액세스 토큰 및 유저 정보 요청
      * @description 로그인 성공 시, 카카오 플랫폼 서버에서 인가 코드 가지고 리다이렉션 (callback) 인가 코드를 사용해서 토큰 발급을 요청
-     *     (getKakaoInfo) 토큰을 사용해서 사용자 정보 가져오기 요청 (getUserInfoWithToken) 사용자 정보를 반환
-     * @return kakaoInfo 유저 정보
+     *     (getKakaoInfo) 토큰을 사용해서 사용자 정보 가져오기 요청 (getUserInfoWithToken) 사용자 정보를 JWT에 담아서 리다이렉트
+     *     (generateJwtToken)
+     * @return JWT 담은 쿠키를 헤더에 담아 리다이렉트
      */
     @GetMapping("/callback/designer")
     public void callbackDesigner(HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         // service - 유저 정보 반환
         KakaoDTO kakaoInfo = kakaoService.getKakaoInfo(request.getParameter("code"), DESIGNER);
+        // service - 토큰 발급
         String jwt = loginService.generateJwtToken(kakaoInfo);
+        // util - 쿠키 생성
+        String cookie = CookieUtil.createCookies(jwt);
 
-        // 쿠키 생성
-        Cookie cookie = new Cookie("token", jwt);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setMaxAge(60 * 60);
-        cookie.setPath("/");
-        response.addCookie(cookie);
-
+        response.setHeader("Set-Cookie", cookie);
         response.sendRedirect(LOGIN_REDIRECT_URL);
     }
 }
