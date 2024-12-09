4ed5935073d3c563626170a84646b61ec5d82da7
rhdqngkwk90 <ouorhdqngkwk90@gmail.com>
2024-12-09 16:14:32 +0900

    Fix: Swagger 수정 + CORS 수정

--- src/main/java/com/ureca/config/web/SwaggerConfig.java
+++ src/main/java/com/ureca/config/web/SwaggerConfig.java
@@ -3,15 +3,27 @@ package com.ureca.config.web;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${server.protocol}")
+    String protocol;
+
+    @Value("${server.host}")
+    String host;
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().components(new Components()).info(apiInfo());
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .addServersItem(new Server().url(protocol + "://" + host).description("https 호스트"))
+                .components(new Components())
+                .info(apiInfo());
     }
 
     private Info apiInfo() {
--- src/main/java/com/ureca/config/web/WebConfig.java
+++ src/main/java/com/ureca/config/web/WebConfig.java
@@ -11,7 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 프론트엔드 도메인에 대해서 CORS를 허용
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("https://www.daeng-ggu.com") // 허용할 도메인
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://www.daeng-ggu-backend.com",
+                        "https://www.daeng-ggu.com") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 허용할 헤더
                 .allowCredentials(true); // 쿠키 등을 허용할지 여부
