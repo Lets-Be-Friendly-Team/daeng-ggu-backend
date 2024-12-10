fe666d13a5d5a5fdc2ddac541bd3be2324250e17
ChoiJiHooon <prin81208@naver.com>
2024-12-10 17:57:47 +0900

    Fix: controller 수정

--- src/main/java/com/ureca/request/presentation/RequestController.java
+++ src/main/java/com/ureca/request/presentation/RequestController.java
@@ -16,7 +16,7 @@ public class RequestController {
 
     private final RequestService requestService;
 
-    @PostMapping("/bid/request/pet")
+    @GetMapping("/bid/request/pet")
     @Operation(summary = "반려견 프로필 조회", description = "[REQ1000] 요청서 작성 페이지에서 반려견 프로필 조회")
     ResponseDto<List<RequestDto.Response>> selectPetProfile() { // TODO : 토큰 수정
         List<RequestDto.Response> responses = requestService.selectPetProfile(2L);
@@ -73,9 +73,9 @@ public class RequestController {
 
     @GetMapping("/bid/request/customer")
     @Operation(summary = "보호자 견적요청서 조회", description = "[REQ3000] 보호자 지난 견적요청서 list 조회.")
-    ResponseDto<String> selectRequestBefore() { // TODO : 토큰 수정
-        requestService.selectRequestBefore(2L);
-        return ResponseUtil.SUCCESS("견적 요청서 리스트 조회가 완료되었습니다.", null);
+    ResponseDto<List<RequestDto.Response>> selectRequestBefore() { // TODO : 토큰 수정
+        List<RequestDto.Response> response = requestService.selectRequestBefore(2L);
+        return ResponseUtil.SUCCESS("견적 요청서 리스트 조회가 완료되었습니다.", response);
     }
 
     /**
