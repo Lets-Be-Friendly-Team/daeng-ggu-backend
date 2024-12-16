fb5d9ff136c1cf463049b14b3b39c94b6d3643d7
heogeonho <geonh1016@gmail.com>
2024-12-16 20:38:42 +0900

    Feat: 변경 요구사항 Swagger에 추가

--- src/main/java/com/ureca/monitoring/presentaion/dto/ReservationInfoForGuardianDto.java
+++ src/main/java/com/ureca/monitoring/presentaion/dto/ReservationInfoForGuardianDto.java
@@ -10,6 +10,8 @@ public class ReservationInfoForGuardianDto {
     private Long reservationId;
     private LocalDate reservationDate;
     private Integer startTime;
+    private Boolean isFinished;
+    private Long processId;
 
     private String customerAddress;
     private String shopAddress;
