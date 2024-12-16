64404376975c9b685e401361861cd498a8845123
JaehyunShin <skwent77@gmail.com>
2024-12-16 18:05:22 +0900

    style: lint 적용

--- src/main/java/com/ureca/config/stream/IVSConfig.java
+++ src/main/java/com/ureca/config/stream/IVSConfig.java
@@ -6,15 +6,14 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ivs.IvsClient;
 
-    @Configuration
+@Configuration
 public class IVSConfig {
 
-        @Bean
-        public IvsClient ivsClient() {
-            return IvsClient.builder()
-                    .region(Region.AP_NORTHEAST_2) // 서울 리전
-                    .credentialsProvider(ProfileCredentialsProvider.create())
-                    .build();
-        }
-
+    @Bean
+    public IvsClient ivsClient() {
+        return IvsClient.builder()
+                .region(Region.AP_NORTHEAST_2) // 서울 리전
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .build();
+    }
 }
