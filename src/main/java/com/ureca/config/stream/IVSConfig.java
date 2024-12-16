032e2f1c1e72cabbc151de52de0129b816d4bbb5
JaehyunShin <skwent77@gmail.com>
2024-12-16 21:35:14 +0900

    feat: IVSClient를 수동으로 빈 등록

--- src/main/java/com/ureca/config/stream/IVSConfig.java
+++ src/main/java/com/ureca/config/stream/IVSConfig.java
@@ -1,3 +1,16 @@
 package com.ureca.config.stream;
 
-public class IVSConfig {}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ivs.IvsClient;
+
+@Configuration
+public class IVSConfig {
+    @Bean
+    public IvsClient ivsClient() {
+        return IvsClient.builder()
+                .region(Region.AP_NORTHEAST_2) // 원하는 AWS 리전으로 설정 (예: 서울 리전)
+                .build();
+    }
+}
