e078c8d572ba797c8d45762ab114b9222f2469dc
heogeonho <geonh1016@gmail.com>
2024-12-17 15:43:34 +0900

    fix: 상태 메세지 수정

--- src/main/java/com/ureca/monitoring/domain/ProcessStatus.java
+++ src/main/java/com/ureca/monitoring/domain/ProcessStatus.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 @Getter
 public enum ProcessStatus {
     PREPARING("서비스를 준비 중입니다"),
-    DELIVERY_TO_SHOP("강아지가 보호자님의 집에서 미용실으로 이동 중입니다"),
+    DELIVERY_TO_SHOP("강아지가 미용실으로 이동 중입니다"),
     WAITING_FOR_GROOMING("강아지가 미용을 기다리고 있습니다"),
     GROOMING("미용이 진행 중입니다"),
-    WAITING_FOR_DELIVERY("미용이 끝나고 집가기를 기다리고 있습니다"),
-    DELIVERY_TO_HOME("강아지가 미용실에서 보호자님의 집으로 이동중입니다"),
+    WAITING_FOR_DELIVERY("강아지가 집가기를 기다리고 있습니다"),
+    DELIVERY_TO_HOME("강아지가 보호자님의 집으로 이동중입니다"),
     COMPLETED("미용이 완료되었어요");
 
     private final String description;
